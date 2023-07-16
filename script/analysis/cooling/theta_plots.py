14018ee05dc8b92b087e0b424002e17310e73268
sammason4 <samason4@illinois.edu>
2023-07-15 21:11:54 -0500

    added theta_plots.py

--- script/analysis/cooling/theta_plots.py
+++ script/analysis/cooling/theta_plots.py
@@ -5,7 +5,7 @@
 #
 ######################################################################
 
-# python3 ./script/analysis/cooling/combined_plots.py -p ./script/analysis/cooling/combined_params.dat
+# python3 ./script/analysis/cooling/theta_plots.py -p ./script/analysis/cooling/theta_params.dat
 #this assumes that both runs have equal grid files
 
 import numpy as np
@@ -26,7 +26,7 @@ def run_parallel(function,dlist,nthreads):
     pool.join()
 
 # Initialize global variables
-globalvars_keys = ['PROB','NDIMS','DUMPSDIR_with','DUMPSDIR_without','PLOTSDIR']
+globalvars_keys = ['PROB','NDIMS','DUMPSDIR_heat','DUMPSDIR_heat_and_cool','PLOTSDIR']
 globalvars = {}
 grid ={}
 
@@ -82,185 +82,213 @@ def xy_slice(var, average=False, patch_phi=False):
         xy_var[:,0] = xy_var[:,-1] = 0
     return xy_var
 
-# Function to overlay field lines
-# Argument must be axes object, B1, B2 and 'nlines' -> a parameter to account for density of field lines
-def plotting_bfield_lines(ax,B1,B2,nlines=20):
-    xp = xz_slice(grid['x'], patch_pole=True)
-    zp = xz_slice(grid['z'])
-    B1_phi_avg = B1.mean(axis=-1) 
-    B2_phi_avg = B2.mean(axis=-1)
-    AJ_phi = np.zeros([2*grid['n1'],grid['n2']]) 
-    for j in range(grid['n2']):
-        for i in range(grid['n1']):
-            AJ_phi[grid['n1']-1-i,j] = AJ_phi[i+grid['n1'],j] = (np.trapz(grid['gdet'][:i,j,0]*B2_phi_avg[:i,j],dx=grid['dx1']) - np.trapz(grid['gdet'][i,:j,0]*B1_phi_avg[i,:j],dx=grid['dx2']))
-    AJ_phi -=AJ_phi.min()
-    levels = np.linspace(0,AJ_phi.max(),nlines*2)
-    ax.contour(xp, zp, AJ_phi, levels=levels, colors='k')
-
-def analysis_torus2d(dumpval, cmap='jet', vmin=-5, vmax=0, domain = [-50,0,-50,50], bh=True, shading='gouraud'):
+def analysis_torus2d(dumpval, cmap='jet', vmin=-6, vmax=-2.5, domain = [-50,0,-50,50], bh=True, shading='gouraud'):
     plt.clf()
     print("Analyzing {0:04d} dump".format(dumpval))
-    dfile = h5py.File(os.path.join(globalvars['DUMPSDIR_with'],'dump_0000{0:04d}.h5'.format(dumpval)),'r')
+    dfile = h5py.File(os.path.join(globalvars['DUMPSDIR_heat'],'dump_0000{0:04d}.h5'.format(dumpval)),'r')
     rho = dfile['prims'][()][Ellipsis,0]
-    uu = np.array(dfile['prims'][()][Ellipsis,1])
-    u = np.array(dfile['prims'][()][Ellipsis,2:5])
-    B = np.array(dfile['prims'][()][Ellipsis,5:8])
-    gam = np.array(dfile['header/gam'][()])
+    game = np.array(dfile['header/gam_e'][()])
+    KEL0 = np.array(dfile['prims'][()][Ellipsis,9])
+    KEL1 = np.array(dfile['prims'][()][Ellipsis,10])
+    KEL2 = np.array(dfile['prims'][()][Ellipsis,11])
+    KEL3 = np.array(dfile['prims'][()][Ellipsis,12])
+    uel0 = rho**game*KEL0/(game-1.)
+    uel1 = rho**game*KEL1/(game-1.)
+    uel2 = rho**game*KEL2/(game-1.)
+    uel3 = rho**game*KEL3/(game-1.)
+    #I would define these at Tel (temperature of the electrons), but at first I tried plotting the normalized temperature, theta,
+    #which ended up being way too small, and I didn't want to go through and change theta to Tel, just for the sake of time
+    theta0 = np.log10((game-1.)*uel0/rho)
+    theta1 = np.log10((game-1.)*uel1/rho)
+    theta2 = np.log10((game-1.)*uel2/rho)
+    theta3 = np.log10((game-1.)*uel3/rho)
     t = dfile['t'][()]
     dfile.close()
     t = "{:.3f}".format(t)
-    logrho=np.log10(rho)
-    pg = (gam-1)*uu
-    gti = grid['gcon'][Ellipsis,0,1:4]
-    gij = grid['gcov'][Ellipsis,1:4,1:4]
-    beta_i = np.einsum('ijks,ijk->ijks',gti,grid['lapse']**2)
-    qsq = np.einsum('ijky,ijky->ijk',np.einsum('ijkxy,ijkx->ijky',gij,u),u)
-    gamma = np.sqrt(1+qsq)
-    ui = u-np.einsum('ijks,ijk->ijks',beta_i,gamma/grid['lapse'])
-    ut = gamma/grid['lapse']
-    ucon = np.append(ut[Ellipsis,None],ui,axis=3)
-    ucov = np.einsum('ijkmn,ijkn->ijkm',grid['gcov'],ucon)
-    bt = np.einsum('ijkm,ijkm->ijk',np.einsum('ijksm,ijks->ijkm',grid['gcov'][Ellipsis,1:4,:],B),ucon)
-    bi = (B+np.einsum('ijks,ijk->ijks',ui,bt))/ut[Ellipsis,None]
-    bcon = np.append(bt[Ellipsis,None],bi,axis=3)
-    bcov = np.einsum('ijkmn,ijkn->ijkm',grid['gcov'],bcon)
-    bsq = np.einsum('ijkm,ijkm->ijk',bcon,bcov)
-    betainv = 0.5*bsq/pg
-    logbetainv = np.log10(betainv)
 
     xp = xz_slice(grid['x'], patch_pole=True)
     zp = xz_slice(grid['z'])
-    rhop = xz_slice(logrho)
-    betainvp = xz_slice(logbetainv)
+    theta0p = xz_slice(theta0)
+    theta1p = xz_slice(theta1)
+    theta2p = xz_slice(theta2)
+    theta3p = xz_slice(theta3)
 
 #**********************************************************************************************************************
 
-    dfile2 = h5py.File(os.path.join(globalvars['DUMPSDIR_without'],'dump_0000{0:04d}.h5'.format(dumpval)),'r')
+    dfile2 = h5py.File(os.path.join(globalvars['DUMPSDIR_heat_and_cool'],'dump_0000{0:04d}.h5'.format(dumpval)),'r')
     rho2 = dfile2['prims'][()][Ellipsis,0]
-    uu2 = np.array(dfile2['prims'][()][Ellipsis,1])
-    u2 = np.array(dfile2['prims'][()][Ellipsis,2:5])
-    B2 = np.array(dfile2['prims'][()][Ellipsis,5:8])
-    gam2 = np.array(dfile2['header/gam'][()])
+    game2 = np.array(dfile2['header/gam_e'][()])
+    KEL02 = np.array(dfile2['prims'][()][Ellipsis,9])
+    KEL12 = np.array(dfile2['prims'][()][Ellipsis,10])
+    KEL22 = np.array(dfile2['prims'][()][Ellipsis,11])
+    KEL32 = np.array(dfile2['prims'][()][Ellipsis,12])
+    uel02 = rho2**game2*KEL02/(game2-1.)
+    uel12 = rho2**game2*KEL12/(game2-1.)
+    uel22 = rho2**game2*KEL22/(game2-1.)
+    uel32 = rho2**game2*KEL32/(game2-1.)
+    theta02 = np.log10((game2-1.)*uel02/rho2)
+    theta12 = np.log10((game2-1.)*uel12/rho2)
+    theta22 = np.log10((game2-1.)*uel22/rho2)
+    theta32 = np.log10((game2-1.)*uel32/rho2)
     t2 = dfile2['t'][()]
     dfile2.close()
     t2 = "{:.3f}".format(t2)
-    logrho2=np.log10(rho2)
-    pg2 = (gam2-1)*uu2
-    gti2 = grid['gcon'][Ellipsis,0,1:4]
-    gij2 = grid['gcov'][Ellipsis,1:4,1:4]
-    beta_i2 = np.einsum('ijks,ijk->ijks',gti2,grid['lapse']**2)
-    qsq2 = np.einsum('ijky,ijky->ijk',np.einsum('ijkxy,ijkx->ijky',gij2,u2),u2)
-    gamma2 = np.sqrt(1+qsq2)
-    ui2 = u2-np.einsum('ijks,ijk->ijks',beta_i2,gamma2/grid['lapse'])
-    ut2 = gamma2/grid['lapse']
-    ucon2 = np.append(ut2[Ellipsis,None],ui2,axis=3)
-    ucov2 = np.einsum('ijkmn,ijkn->ijkm',grid['gcov'],ucon2)
-    bt2 = np.einsum('ijkm,ijkm->ijk',np.einsum('ijksm,ijks->ijkm',grid['gcov'][Ellipsis,1:4,:],B2),ucon2)
-    bi2 = (B2+np.einsum('ijks,ijk->ijks',ui2,bt2))/ut2[Ellipsis,None]
-    bcon2 = np.append(bt2[Ellipsis,None],bi2,axis=3)
-    bcov2 = np.einsum('ijkmn,ijkn->ijkm',grid['gcov'],bcon2)
-    bsq2 = np.einsum('ijkm,ijkm->ijk',bcon2,bcov2)
-    betainv2 = 0.5*bsq2/pg2
-    logbetainv2 = np.log10(betainv2)
 
     xp2 = xz_slice(grid['x'], patch_pole=True)
     zp2 = xz_slice(grid['z'])
-    rhop2 = xz_slice(logrho2)
-    betainvp2 = xz_slice(logbetainv2)
+    theta0p2 = xz_slice(theta02)
+    theta1p2 = xz_slice(theta12)
+    theta2p2 = xz_slice(theta22)
+    theta3p2 = xz_slice(theta32)
 
-    errors = np.array([])
-    rho_reshaped = rho.reshape(16384)
-    uu_reshaped = uu.reshape(16384)
-    u_reshaped = u.reshape(49152)
-    B_reshaped = B.reshape(49152)
-    rho2_reshaped = rho2.reshape(16384)
-    uu2_reshaped = uu2.reshape(16384)
-    u2_reshaped = u2.reshape(49152)
-    B2_reshaped = B2.reshape(49152)
-    errors = np.append(errors, abs(rho_reshaped-rho2_reshaped))
-    errors = np.append(errors, abs(uu_reshaped-uu2_reshaped))
-    errors = np.append(errors, abs(u_reshaped-u2_reshaped))
-    errors = np.append(errors, abs(B_reshaped-B2_reshaped))
-    maxerror = np.amax(errors)
-    maxerror = "{:.16f}".format(maxerror)
-
+    #just to check that there is a difference:
+    max0 = np.amax(theta0.reshape(16384) - theta02.reshape(16384))
+    max1 = np.amax(theta1.reshape(16384) - theta12.reshape(16384))
+    max2 = np.amax(theta2.reshape(16384) - theta22.reshape(16384))
+    max3 = np.amax(theta3.reshape(16384) - theta32.reshape(16384))
+    max0 = "{:.16f}".format(max0)
+    max1 = "{:.16f}".format(max1)
+    max2 = "{:.16f}".format(max2)
+    max3 = "{:.16f}".format(max3)
 
     fig = plt.figure(figsize=(16,9))
-    heights = [1,5]
-    gs = gridspec.GridSpec(nrows=2, ncols=4, height_ratios=heights, figure=fig)
+    heights = [1,5,5]
+    gs = gridspec.GridSpec(nrows=3, ncols=4, height_ratios=heights, figure=fig)
 
     ax0 = fig.add_subplot(gs[0,:])
-    ax0.annotate('t= '+str(t)+' maximum error of any prim = '+str(maxerror),xy=(0.5,0.5),xycoords='axes fraction',va='center',ha='center',fontsize='x-large')
+    ax0.annotate('t= '+str(t)+'; max diff in: temp0: '+str(max0)+', temp1: '+str(max1)+', temp2: '+str(max2)+', temp3: '+str(max3),xy=(0.5,0.5),xycoords='axes fraction',va='center',ha='center',fontsize='x-large')
     ax0.axis("off")
 
-    #plotting rho with:
+    #plotting theta0p:
     ax1 = fig.add_subplot(gs[1,0])
-    rhopolplot = ax1.pcolormesh(xp, zp, rhop, cmap=cmap, vmin=vmin, vmax=vmax, shading=shading)
-    plotting_bfield_lines(ax1,B[Ellipsis,0],B[Ellipsis,1],nlines=40)
+    theta0polplot = ax1.pcolormesh(xp, zp, theta0p, cmap=cmap, vmin=vmin, vmax=vmax, shading=shading)
     ax1.set_xlabel('$x (GM/c^2)$')
     ax1.set_ylabel('$z (GM/c^2)$')
     ax1.set_xlim(domain[:2])
     ax1.set_ylim(domain[2:])
-    ax1.set_title('Log($\\rho$) with COOLING',fontsize='large')
+    ax1.set_title('log(Theta) w/ KEL0; Heating',fontsize='large')
     if bh:
             circle = plt.Circle((0,0),grid['rEH'],color='k')
             ax1.add_artist(circle)
     ax1.set_aspect('equal')
     divider = make_axes_locatable(ax1)
     cax = divider.append_axes("right", size="5%", pad=0.05)
-    plt.colorbar(rhopolplot, cax=cax)
+    plt.colorbar(theta0polplot, cax=cax)
 
-    #plotting rho without:
+    #plotting theta1p:
     ax2 = fig.add_subplot(gs[1,1])
-    rhopolplot2 = ax2.pcolormesh(xp2, zp2, rhop2, cmap=cmap, vmin=vmin, vmax=vmax, shading=shading)
-    plotting_bfield_lines(ax2,B2[Ellipsis,0],B2[Ellipsis,1],nlines=40)
+    theta1polplot = ax2.pcolormesh(xp, zp, theta1p, cmap=cmap, vmin=vmin, vmax=vmax, shading=shading)
     ax2.set_xlabel('$x (GM/c^2)$')
     ax2.set_ylabel('$z (GM/c^2)$')
     ax2.set_xlim(domain[:2])
     ax2.set_ylim(domain[2:])
-    ax2.set_title('Log($\\rho$) without ELECTRONS',fontsize='large')
+    ax2.set_title('log(Theta) w/ KEL1; Heating',fontsize='large')
     if bh:
             circle = plt.Circle((0,0),grid['rEH'],color='k')
             ax2.add_artist(circle)
     ax2.set_aspect('equal')
     divider = make_axes_locatable(ax2)
-    cax2 = divider.append_axes("right", size="5%", pad=0.05)
-    plt.colorbar(rhopolplot2, cax=cax2)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(theta1polplot, cax=cax)
 
-    #plotting betainv with:
+    #plotting theta2p:
     ax3 = fig.add_subplot(gs[1,2])
-    betainvpolplot = ax3.pcolormesh(xp, zp, betainvp, cmap=cmap, vmin=-3, vmax=3, shading=shading)
+    theta2polplot = ax3.pcolormesh(xp, zp, theta2p, cmap=cmap, vmin=vmin, vmax=vmax, shading=shading)
     ax3.set_xlabel('$x (GM/c^2)$')
     ax3.set_ylabel('$z (GM/c^2)$')
     ax3.set_xlim(domain[:2])
     ax3.set_ylim(domain[2:])
-    ax3.set_title('Log($\\beta^{-1}$) with COOLING',fontsize='large')
+    ax3.set_title('log(Theta) w/ KEL2; Heating',fontsize='large')
     if bh:
             circle = plt.Circle((0,0),grid['rEH'],color='k')
             ax3.add_artist(circle)
     ax3.set_aspect('equal')
     divider = make_axes_locatable(ax3)
     cax = divider.append_axes("right", size="5%", pad=0.05)
-    plt.colorbar(betainvpolplot, cax=cax)
+    plt.colorbar(theta2polplot, cax=cax)
 
-    #plotting betainv without:
+    #plotting theta3p:
     ax4 = fig.add_subplot(gs[1,3])
-    betainvpolplot2 = ax4.pcolormesh(xp2, zp2, betainvp2, cmap=cmap, vmin=-3, vmax=3, shading=shading)
+    theta3polplot = ax4.pcolormesh(xp, zp, theta3p, cmap=cmap, vmin=vmin, vmax=vmax, shading=shading)
     ax4.set_xlabel('$x (GM/c^2)$')
     ax4.set_ylabel('$z (GM/c^2)$')
     ax4.set_xlim(domain[:2])
     ax4.set_ylim(domain[2:])
-    ax4.set_title('Log($\\beta^{-1}$) without ELECTRONS',fontsize='large')
+    ax4.set_title('log(Theta) w/ KEL3; Heating',fontsize='large')
     if bh:
             circle = plt.Circle((0,0),grid['rEH'],color='k')
             ax4.add_artist(circle)
     ax4.set_aspect('equal')
     divider = make_axes_locatable(ax4)
-    cax2 = divider.append_axes("right", size="5%", pad=0.05)
-    plt.colorbar(betainvpolplot, cax=cax2)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(theta3polplot, cax=cax)
+
+    #plotting theta0p2:
+    ax5 = fig.add_subplot(gs[2,0])
+    theta02polplot = ax5.pcolormesh(xp, zp, theta0p2, cmap=cmap, vmin=vmin, vmax=vmax, shading=shading)
+    ax5.set_label('$x (GM/c^2)$')
+    ax5.set_ylabel('$z (GM/c^2)$')
+    ax5.set_xlim(domain[:2])
+    ax5.set_ylim(domain[2:])
+    ax5.set_title('log(Theta) w/ KEL0; Heating & Cooling',fontsize='large')
+    if bh:
+            circle = plt.Circle((0,0),grid['rEH'],color='k')
+            ax5.add_artist(circle)
+    ax5.set_aspect('equal')
+    divider = make_axes_locatable(ax5)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(theta02polplot, cax=cax)
+
+    #plotting theta1p2:
+    ax6 = fig.add_subplot(gs[2,1])
+    theta12polplot = ax6.pcolormesh(xp, zp, theta1p2, cmap=cmap, vmin=vmin, vmax=vmax, shading=shading)
+    ax6.set_label('$x (GM/c^2)$')
+    ax6.set_ylabel('$z (GM/c^2)$')
+    ax6.set_xlim(domain[:2])
+    ax6.set_ylim(domain[2:])
+    ax6.set_title('log(Theta) w/ KEL1; Heating & Cooling',fontsize='large')
+    if bh:
+            circle = plt.Circle((0,0),grid['rEH'],color='k')
+            ax6.add_artist(circle)
+    ax6.set_aspect('equal')
+    divider = make_axes_locatable(ax6)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(theta12polplot, cax=cax)
+
+    #plotting theta2p2:
+    ax7 = fig.add_subplot(gs[2,2])
+    theta22polplot = ax7.pcolormesh(xp, zp, theta2p2, cmap=cmap, vmin=vmin, vmax=vmax, shading=shading)
+    ax7.set_label('$x (GM/c^2)$')
+    ax7.set_ylabel('$z (GM/c^2)$')
+    ax7.set_xlim(domain[:2])
+    ax7.set_ylim(domain[2:])
+    ax7.set_title('log(Theta) w/ KEL2; Heating & Cooling',fontsize='large')
+    if bh:
+            circle = plt.Circle((0,0),grid['rEH'],color='k')
+            ax7.add_artist(circle)
+    ax7.set_aspect('equal')
+    divider = make_axes_locatable(ax7)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(theta22polplot, cax=cax)
+
+    #plotting theta3p2:
+    ax8 = fig.add_subplot(gs[2,3])
+    theta32polplot = ax8.pcolormesh(xp, zp, theta3p2, cmap=cmap, vmin=vmin, vmax=vmax, shading=shading)
+    ax8.set_label('$x (GM/c^2)$')
+    ax8.set_ylabel('$z (GM/c^2)$')
+    ax8.set_xlim(domain[:2])
+    ax8.set_ylim(domain[2:])
+    ax8.set_title('log(Theta) w/ KEL3; Heating & Cooling',fontsize='large')
+    if bh:
+            circle = plt.Circle((0,0),grid['rEH'],color='k')
+            ax8.add_artist(circle)
+    ax8.set_aspect('equal')
+    divider = make_axes_locatable(ax8)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(theta32polplot, cax=cax)
 
     plt.tight_layout()
-    plt.savefig(os.path.join(globalvars['PLOTSDIR'],'{}_combined_plot_{:04d}.png'.format(globalvars['PROB'],dumpval)))
+    plt.savefig(os.path.join(globalvars['PLOTSDIR'],'{}_theta_plot_{:04d}.png'.format(globalvars['PROB'],dumpval)))
     plt.close()
 
 # main(): Reads param file, writes grid dict and calls analysis function
@@ -282,14 +310,14 @@ if __name__=="__main__":
         os.makedirs(globalvars['PLOTSDIR'])
 
     # Calculating total dump files
-    dstart = int(sorted(os.listdir(globalvars['DUMPSDIR_with']))[0][-7:-3])
-    dend = int(sorted(list(filter(lambda dump: 'dump' in dump,os.listdir(globalvars['DUMPSDIR_with']))))[-1][-7:-3])
+    dstart = int(sorted(os.listdir(globalvars['DUMPSDIR_heat']))[0][-7:-3])
+    dend = int(sorted(list(filter(lambda dump: 'dump' in dump,os.listdir(globalvars['DUMPSDIR_heat']))))[-1][-7:-3])
     dlist = range(dstart,dend+1)
     Ndumps = dend-dstart+1
 
     # Setting grid dict
-    gfile = h5py.File(os.path.join(globalvars['DUMPSDIR_with'],'grid.h5'),'r')
-    dfile = h5py.File(os.path.join(globalvars['DUMPSDIR_with'],'dump_0000{0:04d}.h5'.format(dstart)),'r')
+    gfile = h5py.File(os.path.join(globalvars['DUMPSDIR_heat'],'grid.h5'),'r')
+    dfile = h5py.File(os.path.join(globalvars['DUMPSDIR_heat'],'dump_0000{0:04d}.h5'.format(dstart)),'r')
     grid['n1'] = dfile['/header/n1'][()]; grid['n2'] = dfile['/header/n2'][()]; grid['n3'] = dfile['/header/n3'][()]
     grid['dx1'] = dfile['/header/geom/dx1'][()]; grid['dx2'] = dfile['/header/geom/dx2'][()]; grid['dx3'] = dfile['/header/geom/dx3'][()]
     grid['startx1'] = dfile['header/geom/startx1'][()]; grid['startx2'] = dfile['header/geom/startx2'][()]; grid['startx3'] = dfile['header/geom/startx3'][()]
